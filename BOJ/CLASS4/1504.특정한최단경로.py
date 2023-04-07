e3d2f9c35ce1b51af789705f538ce20d6fcbb7ea
yunseonyeong <dbstjsdud99@gmail.com>
2023-04-08 02:10:34 +0900

    refactor: BOJ CLASS4 1504. 특정한 최단경로

--- BOJ/CLASS4/1504.특정한최단경로.py
+++ BOJ/CLASS4/1504.특정한최단경로.py
@@ -11,7 +11,7 @@ for _ in range(E):
 
 v1,v2 = map(int, input().split())
 
-def dijkstra(start):
+def dijkstra(start, end):
     distance = [1e9 for _ in range(N+1)]
     queue = []
     heapq.heappush(queue, (0, start))
@@ -19,16 +19,18 @@ def dijkstra(start):
 
     while queue:
         dist, node = heapq.heappop(queue)
+        if node == end:
+            return dist
         # 인접노드 탐색
         for n, d in graph[node]:
             if distance[n] > dist + d:
                 distance[n] = dist + d
                 heapq.heappush(queue, (distance[n], n))
-    
-    return distance
 
-dist_v1 = dijkstra(1)[v1] + dijkstra(v1)[v2] + dijkstra(v2)[N]
-dist_v2 = dijkstra(1)[v2] + dijkstra(v2)[v1] + dijkstra(v1)[N]
+    return distance[end]
+    
+dist_v1 = dijkstra(1, v1) + dijkstra(v1, v2) + dijkstra(v2, N)
+dist_v2 = dijkstra(1, v2) + dijkstra(v2, v1) + dijkstra(v1, N)
 
 if dist_v1 >= 1e9 or dist_v2 >= 1e9:
     print(-1)
